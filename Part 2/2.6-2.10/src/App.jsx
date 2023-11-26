64ea531bfb20656db02266c4968f6e7e17b3c40d
fparedlo <fparedlo@gmail.com>
2023-11-26 00:25:41 +0000

    2.8 + picocss

--- Part 2/2.6-2.10/src/App.jsx
+++ Part 2/2.6-2.10/src/App.jsx
@@ -5,23 +5,35 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newTelf, setNewTelf] = useState(null)
 
   const updateNewName = (event) => {
     setNewName(event.target.value)
   }
 
+  const updateNewTelf = (event) => {
+    setNewTelf(event.target.value)
+  }
+
   const recordName = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      telf: newTelf 
     }
-    const duplicated = persons.filter(person => person.name.toLowerCase() === personObject.name.toLocaleLowerCase())
+    const duplicatedName = persons.filter(person => person.name.toLowerCase() === personObject.name.toLocaleLowerCase())
+    const duplicatedTelf = persons.filter(person => person.telf === personObject.telf)
 
-    if (duplicated.length === 0) {
+    if (duplicatedName.length === 0 && duplicatedTelf.length === 0) {
       setPersons([...persons, personObject])
       setNewName('');
+      setNewTelf(null);
     }else {
-      alert(`${personObject.name} is already added to phonebook`)
+      if(duplicatedName.length > 0){
+        alert(`${personObject.name} is already added to phonebook`)
+      }else {
+        alert(`${personObject.telf} is already added to phonebook`)
+      }
     }
   }
 
@@ -30,7 +42,9 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={recordName}>
         <label for="name">name:</label>
-        <input id="name" value={newName} onChange={updateNewName}/>
+        <input id="name" value={newName} onChange={updateNewName} required/>
+        <label for="telf">Telephone:</label>
+        <input id="telf" type='number' value={newTelf} onChange={updateNewTelf} required/>
         <div>
           <button type="submit">add</button>
         </div>
--- Part 2/2.6-2.10/src/main.jsx
+++ Part 2/2.6-2.10/src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client'
-
+import './../node_modules/@picocss/pico/css/pico.min.css'
 import App from './App'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <main class="container"><App /></main>
 )
