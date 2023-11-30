79fa3f93a4f25dd0585e583166c6bb58c1540139
fparedlo <fparedlo@gmail.com>
2023-11-30 16:08:41 +0000

    regular save

--- Part 2/2.18-2.20/src/components/Results.jsx
+++ Part 2/2.18-2.20/src/components/Results.jsx
@@ -7,37 +7,34 @@ const Results = ({ filtered }) => {
       }
 
       {
-        (filtered.length < 10 && filtered.length > 1) &&
+        filtered.length < 10 && filtered.length > 1 &&
         <ul>
           {
-            filtered.map(country => <li key={country.name.common}>{country.name.common}</li>)
+            filtered.map(country => <li key={country.population}>{country.name.common} <button>show</button></li>)
           }
         </ul>
       }
 
       {
         filtered.length === 1 &&
-        filtered.map(country => (
-          <>
-            <h3 key={country.name.common}>{country.name.common}</h3>
-            <ul>
-              <li>Capital: {country.capital}</li>
-              <li>Area: {country.area}</li>
-              <li>Languages:
-                <ul>
-                  {
-                    Object.entries(country.languages).map(([key, value]) => <li key={key}>{value}</li>)
-                  }
-                </ul>
-              </li>
-              <li>Flag: <img src={country.flags.svg} alt={country.flags.alt} width="100" /></li>
-            </ul>
-          </>
-        )
-        )
+        <>
+          <h3>{filtered[0].name.common}</h3>
+          <ul>
+            <li>Capital: {filtered[0].capital}</li>
+            <li>Area: {filtered[0].area}</li>
+            <li>Languages:
+              <ul>
+                {
+                  Object.entries(filtered[0].languages).map(([key, value]) => <li key={key}>{key} {value}</li>)
+                }
+              </ul>
+            </li>
+            <li>Flag: <img src={filtered[0].flags.svg} alt={filtered[0].flags.alt} width="100" /></li>
+          </ul>
+        </>
       }
     </>
   )
 }
 
-export default Results
+      export default Results
