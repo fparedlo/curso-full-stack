751263f31f7e12cf8a32e2fdd967a61f42ff861c
fparedlo <fparedlo@gmail.com>
2023-12-06 00:23:02 +0000

    following the lessons

---
+++ Part 3/Lessons/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  },
+  {
+    id: 4,
+    content: "one more to test nodemon",
+    important: false
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if(!note) response.status(404).send('not found')
+  response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id)) 
+    : 0
+  const note = request.body
+  note.id = maxId + 1
+
+
+  notes = [...notes, note]
+  response.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`EXPRESS server running in port ${PORT}`))
